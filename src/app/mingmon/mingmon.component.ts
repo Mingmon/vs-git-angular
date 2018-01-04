cfd6fc20927e2a15ffd6c966b332485a1bc542f6
Mingmon Suay <60010313@kmitl.ac.th>
2018-01-04 21:57:38 +0700

    *ngIf Local Variable Assignment

--- src/app/mingmon/mingmon.component.ts
+++ src/app/mingmon/mingmon.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MingmonComponent implements OnInit {
   visible = true;
+  myFavLang = {
+      'frontend' : ['html','css','javascript'],
+      'backend' : ['nodejs','php','python','ruby']
+  }
   toggleDiv(){
     this.visible = !this.visible;
   }
+  counter =0;
 
   constructor() { }
 
